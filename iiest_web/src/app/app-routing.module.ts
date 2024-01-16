afa6cb4ea6ae43f1e14adc196b491c6c74d40f6d
vansh <vanshgora31@gmail.com>
2024-01-16 13:32:50 +0530

    started settings Pannel

--- iiest_web/src/app/app-routing.module.ts
+++ iiest_web/src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { authGuard } from './shared/gaurds/auth.guard';
 import { routeGuard } from './shared/gaurds/route.guard';
 import { FbonewComponent } from './pages/fboproduct/fbonew/fbonew.component';
 import { fbo_roles, empRegister_roles } from './utils/config';
+import { SettingPanelComponent } from './shared/setting-panel/setting-panel.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' }, // Default route
   { path: 'main', component: LandingpageComponent},
   { path: 'home', component: HomeComponent, canActivate:[authGuard]},
+  { path: 'settings', component: SettingPanelComponent, canActivate:[authGuard]},
   { path: 'empregister', component: SignupComponent, canActivate:[authGuard, routeGuard], data: {allowedRoles:empRegister_roles}},
   { path: 'fbo', component: FbonewComponent, canActivate:[authGuard, routeGuard], data: {allowedRoles:fbo_roles}}
 ];
--- iiest_web/src/app/shared/setting-panel/setting-panel.component.ts
+++ iiest_web/src/app/shared/setting-panel/setting-panel.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-setting-panel',
   templateUrl: './setting-panel.component.html',
   styleUrls: ['./setting-panel.component.scss']
 })
-export class SettingPanelComponent {
+export class SettingPanelComponent implements OnInit {
+  submitted:boolean = false;
+  updateProfileForm:FormGroup = new FormGroup({
+    userImage:new FormControl(''),
+    userSignature:new FormControl('')
+  })
 
+  constructor(private formBuilder:FormBuilder){
+    
+  }
+
+  ngOnInit(): void {
+    this.updateProfileForm=this.formBuilder.group({
+      userImage:['',[Validators.required]],
+      userSignature:['',[Validators.required]]
+    })
+  }
+
+  get updateprofileform(): { [key: string]: AbstractControl } {
+    return this.updateProfileForm.controls;
+  }
 }
