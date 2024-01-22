b1d3c9fd12283b4283e36e1e8599ef96b61d1153
Bhavesh Daipuria <bdaipuria@gmail.com>
2024-01-22 17:27:39 +0530

    redid changes in caseinfo

--- iiest-server/controllers/operationControllers/caseList.js
+++ iiest-server/controllers/operationControllers/caseList.js
@@ -34,14 +34,6 @@ exports.caseInfo = async(req, res)=>{
         const saleId = req.params.id;
 
         let salesInfo = await salesModel.findById(saleId);
-
-        if(!salesInfo){
-            salesInfo = await recipientModel.findById(saleId);
-            if(!salesInfo){
-                success = false;
-                return res.status(404).json({randomErr: true})
-            }
-        }
         
         const moreInfo = await salesInfo.populate(['fboInfo', 'employeeInfo'])
 
