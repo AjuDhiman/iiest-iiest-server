bc49e560da42c0b9dbda22586b8ef79061137279
vansh <vanshgora31@gmail.com>
2024-04-09 16:07:50 +0530

    come changes in revert section and change ui of exsisting user search

--- iiest_web/src/app/utils/config.ts
+++ iiest_web/src/app/utils/config.ts
@@ -16,6 +16,60 @@ export const clientType = ['General Client', 'Corporate Client'];
 //Payment Mode
 export const paymentMode = ['Cash', 'Pay Page'];
 
+export const auditers = [
+   'Aditi',
+   'Umar'
+];
+
+export const trainers = [
+   'Aditi',
+   'Amit Khanna',
+   'Vijay Saini',
+   'Vinay Kumar Stphene',
+   'Yashwanth Murthy',
+   'Yashodha Devi Palkna',
+   'Harshvardhan Tmrakr',
+   'Praduman Kumar'
+];
+
+export const venues = [
+   {
+      name: 'IIEST',
+      vendor: 'IIEST',
+      location: 'Delhi'
+   },
+   {
+      name: 'Imperial Banquet',
+      vendor: 'Greenvilley',
+      location: 'Gurugram'
+   },
+   {
+      name: 'Indian Resort and Restaurant',
+      vendor: 'Indian Resort and Restaurant',
+      location: 'Faridabad'
+   },
+   {
+      name: 'Hotel Howdy',
+      vendor: 'Hotel Howdy',
+      location: 'Faridabad'
+   },
+   {
+      name: 'Radient Hospitality',
+      vendor: 'Radient Hospitality',
+      location: 'Noida'
+   },
+   {
+      name: 'Hotel Samrath',
+      vendor: 'Perfect Celebrations',
+      location: 'Kaushambi'
+   },
+   {
+      name: 'Yo Food',
+      vendor: 'Parky HVAC India Pvt. Ltd.',
+      location: 'Delhi'
+   },
+]
+
 export const ownershipType = [
    'Propraitorship',
    'Partnership',
