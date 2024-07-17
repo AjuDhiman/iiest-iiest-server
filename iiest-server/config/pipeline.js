8984283d3445cfe10079ff28c84e49baad7fef43
vansh <vanshgora31@gmail.com>
2024-07-17 10:44:20 +0530

    changes in pipeline.js

--- iiest-server/config/pipeline.js
+++ iiest-server/config/pipeline.js
@@ -1,5 +1,28 @@
 //this file contains all the general pipelines for aggregation
 
+// ----------------------------------------------------- vars related to pielines  -------------------------------------------------------
+
+
+
+//var for timelines usually used in getting for a particular time Interval
+const todayDate = new Date(); // getting today's date string
+const startOfToday = new Date(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate()); //getting time of start of the day
+const startOfThisWeek = new Date(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate() - todayDate.getDay());//getting time of start of this weeek
+const startOfPrevMonth = new Date(todayDate.getFullYear(), todayDate.getMonth() - 1, 1);//getting time of start of prev month
+const startOfThisMonth = new Date(todayDate.getFullYear(), todayDate.getMonth(), 1);//getting time of start of this month
+const startOfThisFinancialYear = new Date(todayDate.getFullYear(), 3, 1); //getting time of start of this year
+
+
+
+
+
+
+
+
+// =================================================================================PIPE LINES======================================================================================
+
+
+
 //pipeline for calculating fotac Revenue 
 //It follows the formula of revenue = (fostac processing amount * no. of recps) in that sale
 const fostacRevenue = [
@@ -18,27 +41,32 @@ const fostacRevenue = [
 //It follows the formula of revenue = (foscos processing amount * no. of shops + water test amount in foscos - 1500(only in case of water test fee with foscos != 0)) in that sale
 const foscosRevenue = [
     {
-        $multiply: [
-            { $toInt: { $ifNull: ["$foscosInfo.foscos_processing_amount", 0] } },
-            { $toInt: { $ifNull: ["$foscosInfo.shops_no", 0] } },
-        ]
-    },
-    {
-        $toInt: { $ifNull: ["$foscosInfo.water_test_fee", 0] },
-    },
-    {
-        $cond: [
+        $sum: [
             {
-                $and: [
-                    { $gt: [{ $type: '$foscosInfo' }, 'missing'] },
-                    { $ne: ['$foscosInfo.water_test_fee', '0'] }
+                $multiply: [
+                    { $toInt: { $ifNull: ["$foscosInfo.foscos_processing_amount", 0] } },
+                    { $toInt: { $ifNull: ["$foscosInfo.shops_no", 0] } },
                 ]
             },
-            -1200,
-            0
+            {
+                $toInt: { $ifNull: ["$foscosInfo.water_test_fee", 0] },
+            },
+            {
+                $cond: [
+                    {
+                        $and: [
+                            { $ne: [{ $type: '$foscosInfo' }, 'missing'] },
+                            { $ne: [{ $toInt: "$foscosInfo.water_test_fee" }, 0] }
+                        ]
+                    },
+                    -1200,
+                    0
+                ]
+            },
         ]
-    },
+    }
 ];
+
 
 
 
@@ -112,8 +140,8 @@ const waterTestRevenue = [
 //------------------------------------------------------Conditionl Pipelines----------------------------------------------------------------------
 
 
-//Sales approval condition
-const isSalesApproved = [
+//Pending Sales condition
+const salesPendingCond = [
     {
         $or: [
             // Condition 1: cheque_data exists and cheque_data.status is "Pending"
@@ -129,4 +157,20 @@ const isSalesApproved = [
     }
 ]
 
-module.exports = { fostacRevenue, foscosRevenue, hraRevenue, medicalRevenue, waterTestRevenue }
+
+//Approved Sales condition
+const salesApprovalCond = [
+    {
+        $and: [
+            {
+                $or: [
+                    { $eq: [{ $ifNull: ["$cheque_data", null] }, null] }, // Check if cheque_data is null or doesn't exist
+                    { $eq: [{ $ifNull: ["$cheque_data.status", null] }, "Approved"] } // Check if cheque_data.status is "Approved"
+                ]
+            },
+            { $eq: ["$fboInfo.isBasicDocUploaded", true] }
+        ]
+    }
+]
+
+module.exports = { startOfToday, startOfThisWeek, startOfThisMonth, startOfPrevMonth, startOfThisFinancialYear, fostacRevenue, foscosRevenue, hraRevenue, medicalRevenue, waterTestRevenue, salesPendingCond, salesApprovalCond }
