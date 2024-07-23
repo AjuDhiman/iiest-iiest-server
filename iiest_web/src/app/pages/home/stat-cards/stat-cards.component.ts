883b1efb63fda40064d0e3e827d624c28f59dcbf
vansh <vanshgora31@gmail.com>
2024-07-23 08:12:42 +0530

    some bug fixed

--- iiest_web/src/app/pages/home/stat-cards/stat-cards.component.ts
+++ iiest_web/src/app/pages/home/stat-cards/stat-cards.component.ts
@@ -5,7 +5,7 @@ import { RegisterService } from 'src/app/services/register.service';
 import { IconDefinition, faIndianRupeeSign } from '@fortawesome/free-solid-svg-icons';
 import { DepartmentListComponent } from 'src/app/pages/modals/department-list/department-list.component';
 import { ToastrService } from 'ngx-toastr';
-import { Months } from 'src/app/utils/config';
+import { director_roles, Months } from 'src/app/utils/config';
 
 @Component({
   selector: 'app-stat-cards',
@@ -15,19 +15,24 @@ import { Months } from 'src/app/utils/config';
 export class StatCardsComponent implements OnInit {
   departmentAndCount: Array<{ department: string, count: string, active: string, inactive: string }>
   department: string = '';
+  designation: string = '';
   salesData: any;
+  ticketData: any;
   faIndianRupeeSign: IconDefinition = faIndianRupeeSign;
   Months: string[] = Months; // this var is comming from config.ts and it is a array of all months 
   thisMonth: String = Months[new Date().getMonth()];
   prevMonth: String = Months[new Date().getMonth() - 1];
 
+  //director roles
+  directorRoles: string[] = director_roles;
+
   @Output() emitDeptCount: EventEmitter<any> = new EventEmitter<any>
 
 
-  constructor(private _getDataService:GetdataService,
-    private _registerService:RegisterService,
+  constructor(private _getDataService: GetdataService,
+    private _registerService: RegisterService,
     private _toastrService: ToastrService,
-    private modalService: NgbModal){
+    private modalService: NgbModal) {
   }
 
   ngOnInit(): void {
@@ -37,26 +42,36 @@ export class StatCardsComponent implements OnInit {
     let loggedInUserData: any = this._registerService.LoggedInUserData();
     loggedInUserData = JSON.parse(loggedInUserData)
     this.department = loggedInUserData.department;
+    this.designation = loggedInUserData.designation;
+
   }
 
-  getUserRecord():void {
+  getUserRecord(): void {
+
     this._getDataService.getUserRecord().subscribe({
       next: (res) => {
         this.salesData = res[0];
       }
-    })
+    });
+
+    // this._getDataService.getTicketVerificationData().subscribe({
+    //   next: res => {
+    //     this.ticketData = res[0];
+    //     console.log(this.ticketData)
+    //   }
+    // })
   }
 
-  getEmployeeCountByDept(): void{
+  getEmployeeCountByDept(): void {
     this._getDataService.getEmpCount().subscribe({
-      next : res =>{
-        let departmentArr=res.employeeGroupCount;
-        this.departmentAndCount=departmentArr.map((elem:any) => {
-          return{
-            department:elem._id.department,
-            count:elem.count,
-            active:elem.active,
-            inactive:elem.inactive
+      next: res => {
+        let departmentArr = res.employeeGroupCount;
+        this.departmentAndCount = departmentArr.map((elem: any) => {
+          return {
+            department: elem._id.department,
+            count: elem.count,
+            active: elem.active,
+            inactive: elem.inactive
           }
         });
         this.departmentAndCount = this.departmentAndCount.sort((a: any, b: any) => a.department > b.department ? 1 : -1);
@@ -68,13 +83,13 @@ export class StatCardsComponent implements OnInit {
     })
   }
 
-  viewDepartmentData(res:any): void{
+  viewDepartmentData(res: any): void {
     const modalRef = this.modalService.open(DepartmentListComponent, { size: 'lg', backdrop: 'static' });
     modalRef.componentInstance.department = res;
   }
 
-  changeNameFormat(str:any): string {
-    let updatedStr: string =  str.toLocaleLowerCase().split(" ").join('_');
+  changeNameFormat(str: any): string {
+    let updatedStr: string = str.toLocaleLowerCase().split(" ").join('_');
     updatedStr = updatedStr.replace('&', 'and');
     return updatedStr;
   }
